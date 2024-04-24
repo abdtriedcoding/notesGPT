59326c49300ee336fda22fdc71b6c43f4c579bf9
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-04-24 11:36:30 +0530

    update font and global.css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/navbar";
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 
-const inter = Inter({ subsets: ["latin"] });
+import Navbar from "@/components/navbar";
+
+const font = Poppins({ subsets: ["latin"], weight: ["500"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={font.className}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
