aa3de6090670c8293df94230bb8fbf58714d63f5
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-04-27 00:04:43 +0530

    add shadcn table

---
+++ app/(dashboard)/recordings/_components/data-table-view-options.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { Table } from "@tanstack/react-table";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { MoreHorizontalIcon } from "lucide-react";
+
+interface DataTableViewOptionsProps<TData> {
+  table: Table<TData>;
+}
+
+export function DataTableViewOptions<TData>({
+  table,
+}: DataTableViewOptionsProps<TData>) {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="outline"
+          size="sm"
+          className="ml-auto hidden h-8 lg:flex"
+        >
+          <MoreHorizontalIcon className="mr-2 h-4 w-4" />
+          View
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-[150px]">
+        <DropdownMenuLabel>Toggle columns</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        {table
+          .getAllColumns()
+          .filter(
+            (column) =>
+              typeof column.accessorFn !== "undefined" && column.getCanHide()
+          )
+          .map((column) => {
+            return (
+              <DropdownMenuCheckboxItem
+                key={column.id}
+                className="capitalize"
+                checked={column.getIsVisible()}
+                onCheckedChange={(value) => column.toggleVisibility(!!value)}
+              >
+                {column.id}
+              </DropdownMenuCheckboxItem>
+            );
+          })}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
--- app/(dashboard)/recordings/page.tsx
+++ app/(dashboard)/recordings/page.tsx
@@ -1,3 +1,32 @@
-export default function RecordingPage() {
-  return <div>RecordingPage</div>;
+import path from "path";
+import { promises as fs } from "fs";
+import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/columns";
+
+async function getTasks() {
+  const data = await fs.readFile(path.join(process.cwd(), "/tasks.json"));
+
+  const tasks = JSON.parse(data.toString());
+
+  return tasks;
+}
+
+export default async function RecordingPage() {
+  const tasks = await getTasks();
+
+  return (
+    <>
+      <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
+        <div className="flex items-center justify-between space-y-2">
+          <div>
+            <h2 className="text-2xl font-bold tracking-tight">Welcome back!</h2>
+            <p className="text-muted-foreground">
+              Here&apos;s a list of your tasks for this month!
+            </p>
+          </div>
+        </div>
+        <DataTable data={tasks} columns={columns} />
+      </div>
+    </>
+  );
 }
