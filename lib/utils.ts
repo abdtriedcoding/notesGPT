d645c1cdd3067d89ab438d37f3c7a543127d7ae3
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-05-03 01:33:56 +0530

    update: metadata url

--- lib/utils.ts
+++ lib/utils.ts
@@ -35,7 +35,7 @@ export function constructMetadata({
       images: [image],
       creator: "@abdtriedcoding",
     },
-    metadataBase: new URL("https://notesgpt.vercel.app"),
+    metadataBase: new URL("https://notessgpt.vercel.app"),
   };
 }
 
