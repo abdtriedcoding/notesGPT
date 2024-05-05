3008f9533fd3adfaddae9eab3770c805da6d5c53
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-05-05 15:56:50 +0530

    add: empty state section

--- app/(dashboard)/recordings/page.tsx
+++ app/(dashboard)/recordings/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 
+import Link from "next/link";
 import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
+import { Mic, PencilLine } from "lucide-react";
 import { columns } from "./_components/columns";
 import { DataTable } from "./_components/data-table";
 import { TableSkelton } from "@/components/skeltons";
+import { buttonVariants } from "@/components/ui/button";
 
 export default function RecordingPage() {
   const userNotes = useQuery(api.notes.getUserNotes);
@@ -21,7 +24,34 @@ export default function RecordingPage() {
           Here&apos;s a list of all your notes!
         </p>
       </div>
-      <DataTable data={userNotes} columns={columns} />
+      {userNotes.length === 0 ? (
+        <EmptyState />
+      ) : (
+        <DataTable data={userNotes} columns={columns} />
+      )}
     </>
+  );
+}
+
+function EmptyState() {
+  return (
+    <div className="py-24 flex flex-col items-center justify-center">
+      <div className="flex flex-col lg:flex-row gap-4">
+        <Link
+          href="/record"
+          className={buttonVariants({ variant: "default", size: "lg" })}
+        >
+          <span>Record a New Voice Note</span>
+          <Mic className="ml-2 h-4 w-4" />
+        </Link>
+        <Link
+          href="/action-items"
+          className={buttonVariants({ variant: "secondary", size: "lg" })}
+        >
+          <span>View Action Items</span>
+          <PencilLine className="ml-2 h-4 w-4" />
+        </Link>
+      </div>
+    </div>
   );
 }
